78129a8ceef6e7ea81960b135646bd541197a118
Miguel Madero <mmadero@zenefits.com>
2016-03-31 16:25:25 -0300

    Moves broccoli-funnel and merge-tress to from devdeps to dependencies and removes unnecessary install step for get-config since it’s a package dependency.

--- blueprints/ember-cli-bundle-loader/index.js
+++ blueprints/ember-cli-bundle-loader/index.js
@@ -1,13 +1,6 @@
 /*jshint node:true*/
 module.exports = {
   description: 'Adds configuration files required by ember-cli-bundle-loader',
-  afterInstall: function () {
-    var task = this.taskFor('addon-install');
-    return task.run({
-      verbose: true,
-      packages: ['ember-get-config']
-    });
-  },
   normalizeEntityName: function() {
     // Normalize and validate entity name here.
     // The entitiy name isn't required, but ember-cli crashes without it
