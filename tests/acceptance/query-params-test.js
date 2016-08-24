8ec24cdac7791154bd60f8d114bccf42cee82462
Miguel Madero <git@miguelmadero.com>
2016-08-24 16:07:09 -0300

    Retunrs the last async helper to make sure we wait for the test to complete

--- tests/acceptance/query-params-test.js
+++ tests/acceptance/query-params-test.js
@@ -12,5 +12,5 @@ test('Query params are not stored in cache for bundles that are not yet loaded',
   visit('link-source');
   andThen(() =>
     assert.equal(find('.link-source a').attr('href'), '/link-target?page=2&sort=DESC'));
-  visit('/'); // reset this so refreshing the browser starts at the root
+  return visit('/'); // reset this so refreshing the browser starts at the root
 });
