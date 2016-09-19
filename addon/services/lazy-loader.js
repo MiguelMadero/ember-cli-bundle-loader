52d842d514f97a0d865dffda2b54f018f6a8d96c
Miguel Madero <git@miguelmadero.com>
2016-09-19 09:06:19 -0700

    Extracts the loadAsset functionality to a new util and makes sure we don’t issue new request if there is one in-flight

--- addon/services/lazy-loader.js
+++ addon/services/lazy-loader.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import bundles from 'ember-cli-bundle-loader/config/bundles';
 import { getContainer } from 'ember-cli-bundle-loader/utils/get-owner';
+import loadAssets from 'ember-cli-bundle-loader/utils/load-assets';
 
 const A = Ember.A;
 let loadedBundles = {};
@@ -50,7 +51,7 @@ export default Ember.Service.extend({
       return Ember.RSVP.resolve();
     }
 
-    return this._loadAssets(bundle).then(()=>this.markBundleAsLoaded(bundle.name));
+    return loadAssets(bundle.urls).then(()=>this.markBundleAsLoaded(bundle.name));
   },
 
   _getRouteNameFromUrl (url) {
@@ -59,38 +60,5 @@ export default Ember.Service.extend({
     if (routes && routes.length) {
       return routes[routes.length-1].handler;
     }
-  },
-
-  _loadAssets (bundle) {
-    const urls = bundle.urls || [];
-    const promises = urls.map(url=>
-      url.match(/\.js$/) ?
-        this._loadScript(url) :
-        url.match(/\.css$/) ?
-          this._loadStylesheet(url) :
-          Ember.RSVP.reject(`The specified url (${url}) for bundle ${bundle.name} doesnt match any of the expected types`)
-    );
-    return Ember.RSVP.all(promises);
-  },
-
-  // TODO: extract to a util.
-  _loadScript (url) {
-    var scriptElement = Ember.$("<script>").prop({src: url, async: true});
-    let promise = new Ember.RSVP.Promise((resolve, reject)=>{
-      scriptElement.one('load', ()=> Ember.run(null, resolve));
-      scriptElement.one('error', (evt)=> Ember.run(null, reject, evt));
-    });
-    document.head.appendChild(scriptElement[0]);
-    return promise;
-  },
-  // TODO: extract to a util.
-  _loadStylesheet (url) {
-    let linkElement = Ember.$(`<link rel="stylesheet" href="${url}" type="text/css"/>`);
-    let promise = new Ember.RSVP.Promise((resolve, reject)=>{
-      linkElement.one('load', ()=> Ember.run(null, resolve));
-      linkElement.one('error', (evt)=> Ember.run(null, reject, evt));
-    });
-    document.head.appendChild(linkElement[0]);
-    return promise;
   }
 });
