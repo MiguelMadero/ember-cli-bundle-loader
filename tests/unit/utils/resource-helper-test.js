540b151f6218fefc07ff62e20f3483f83df2b54e
Miguel Madero <mmadero@zenefits.com>
2017-01-25 13:43:53 -0800

    Yarn updates to update yarn.lock, updates approved files based on the latest build output and moves the tests to the right place and renames a few internal members

--- tests/unit/utils/resource-helper-test.js
+++ tests/unit/utils/resource-helper-test.js
@@ -1,14 +1,9 @@
-import Ember from 'ember';
-import sinonTest from 'ember-sinon-qunit/test-support/test';
-import { module } from 'qunit';
-
-import loadAssets from 'ember-cli-bundle-loader/utils/load-assets';
-import ResourceHelper from 'ember-cli-bundle-loader/utils/resource-helper';
-
+import { module, test } from 'qunit';
+import ResourceHelper from 'ember-cli-bundle-loader/utils/-resource-helper';
 
 module('Unit | Utility | resource loader');
 
-sinonTest('loadJavascript adds the appropriate script tag to the DOM', function (assert) {
+test('loadJavascript adds the appropriate script tag to the DOM', function (assert) {
 
   let testAsset = 'assets/load-assets-test2.js';
 
@@ -19,7 +14,15 @@ sinonTest('loadJavascript adds the appropriate script tag to the DOM', function 
   });
 });
 
-sinonTest('loadStylesheet adds the appropriate stylesheet link to the DOM', function (assert) {
+test('loadJavascript. The resource will not be considered as loaded if it failed', function (assert) {
+  let testAsset = 'assets/dosntexist.js';
+
+  return ResourceHelper.loadJavascript(testAsset).finally(function() {
+    assert.equal(false, ResourceHelper.isJavascriptLoaded(testAsset));
+  });
+});
+
+test('loadStylesheet adds the appropriate stylesheet link to the DOM', function (assert) {
   let testAsset = 'assets/load-assets-test2.css';
 
   assert.equal(false, ResourceHelper.isStylesheetLoaded(testAsset));
@@ -29,38 +32,10 @@ sinonTest('loadStylesheet adds the appropriate stylesheet link to the DOM', func
   });
 });
 
-sinonTest('loadAssets wont load an already loaded script', function (assert) {
-  this.stub(ResourceHelper, 'isJavascriptLoaded').returns(true);
-  var loaderSpy = this.spy(ResourceHelper, 'loadJavascript');
+test('loadStylesheet. The resource will not be considered as loaded if it failed', function (assert) {
+  let testAsset = 'assets/dosntexist.css';
 
-  loadAssets(['assets/doesntmatter.js']);
-
-  assert.notOk(loaderSpy.called);
-});
-
-sinonTest('loadAssets wont load an already loaded stylesheet', function (assert) {
-  this.stub(ResourceHelper, 'isStylesheetLoaded').returns(true);
-  var loaderSpy = this.spy(ResourceHelper, 'loadStylesheet');
-
-  loadAssets(['assets/doesntmatter.css']);
-
-  assert.notOk(loaderSpy.called);
-});
-
-sinonTest('loadAssets calls the loader for javascript', function (assert) {
-  this.stub(ResourceHelper, 'isJavascriptLoaded').returns(false);
-  var loaderStub = this.stub(ResourceHelper, 'loadJavascript').returns(Ember.RSVP.resolve());
-
-  loadAssets(['assets/doesntmatter.js']);
-
-  assert.ok(loaderStub.called);
-});
-
-sinonTest('loadAssets calls the loader for stylesheets', function (assert) {
-  this.stub(ResourceHelper, 'isStylesheetLoaded').returns(false);
-  var loaderStub = this.stub(ResourceHelper, 'loadStylesheet').returns(Ember.RSVP.resolve());
-
-  loadAssets(['assets/doesntmatter.css']);
-
-  assert.ok(loaderStub.called);
+  return ResourceHelper.loadStylesheet(testAsset).finally(function() {
+    assert.equal(false, ResourceHelper.isStylesheetLoaded(testAsset));
+  });
 });
